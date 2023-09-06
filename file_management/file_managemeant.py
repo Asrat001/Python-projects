447542b2de2151da30d9ecf01c46e3a13af60a36
assrat007 <asratadane169@gmail.com>
2023-09-06 21:36:16 +0300

    after rebase

--- file_management/file_managemeant.py
+++ file_management/file_managemeant.py
@@ -1,12 +1,35 @@
-import os
+from os import scandir ,rename
+from os.path import exists , splitext, join
+from  shutil import move
+from time import sleep
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 source_dir ="/home/asrat/Downloads" 
 dist_dir_img ="/home/asrat/Downloads/images"
 dist_dir_pdf="/home/asrat/Downloads/pdfs"
 dist_dir_music="/home/asrat/Downloads/musics"
 dist_dir_video="/home/asrat/Downloads/videos"
 
-with os.scandir(source_dir) as enteries:
-        for entry in enteries:
-                print(entry)
 
 
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        print("heyee")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
